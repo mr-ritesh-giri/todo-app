e500942266150356d42e4b0138210ab3a9534a9d
Ritesh Giri <rrgiri032@gmail.com>
2024-05-29 11:04:00 +0530

    UI done.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("Hi there")
+const totalTaskNo = document.getElementById("total-tasks-no").value;
+const totalTaskComp = document.getElementById("task-completed-no").value;
+const cancelInput = document.getElementsByClassName("cancel-input").value;
+const deleteTodo = document.getElementsByClassName("delete-todo").value;
+
+const todoInput = document.getElementById("input-task").value;
+
+function newTodo() {
+    
+}
